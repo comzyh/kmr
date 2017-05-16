85cfeedbf96738f04a02b12092ad51515d08acbe
Comzyh <comzyh@gmail.com>
2017-05-16 12:09:48 +0800

    wordcount example

--- examples/wordcount/main.go
+++ examples/wordcount/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
+	"unicode"
 
 	kmrpb "github.com/naturali/kmr/compute/pb"
 	"google.golang.org/grpc"
@@ -16,19 +20,58 @@ const (
 type wordCountSerer struct{}
 
 func (s *wordCountSerer) ConfigMapper(stream kmrpb.Compute_ConfigMapperServer) error {
-	return nil
+	return stream.SendAndClose(&kmrpb.ConfigResponse{Retcode: 0})
 }
 
 func (s *wordCountSerer) ConfigReducer(stream kmrpb.Compute_ConfigReducerServer) error {
-	return nil
+	return stream.SendAndClose(&kmrpb.ConfigResponse{Retcode: 0})
 }
 
 func (s *wordCountSerer) Map(stream kmrpb.Compute_MapServer) error {
-	return nil
+	wordMap := make(map[string]int)
+	for {
+		kv, err := stream.Recv()
+		if err == io.EOF {
+			for key, value := range wordMap {
+				stream.Send(&kmrpb.KV{Key: []byte(key), Value: []byte(strconv.Itoa(value))})
+			}
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		for _, key := range strings.FieldsFunc(string(kv.Value), func(c rune) bool {
+			return !unicode.IsLetter(c)
+		}) {
+			wordMap[key]++
+		}
+	}
 }
 
 func (s *wordCountSerer) Reduce(stream kmrpb.Compute_ReduceServer) error {
-	return nil
+	word := ""
+	count := 0
+	emit := func() {
+		stream.Send(&kmrpb.KV{Key: []byte(word), Value: []byte(strconv.Itoa(count))})
+	}
+	for {
+		kv, err := stream.Recv()
+		if err == io.EOF {
+			emit()
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if string(kv.Key) == word {
+			c, _ := strconv.Atoi(string(kv.Value))
+			count += c
+		} else {
+			emit()
+			word = string(kv.Key)
+			count = 1
+		}
+	}
 }
 
 func main() {
